4c19604393dcd79aed31a92cbf7f23d330368138
PRnyc <princeagyapong941@gmail.com>
2021-05-02 12:58:22 +0200

    Add files via upload

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,7 +1,7 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from the site 'You Site Name'"; //letter subject
-	$to = 'beshleygame@mail.ru'; // Enter Your E-mail
+	$subj = "New message from the site 'PA website'"; //letter subject
+	$to = 'princeagyapong941@gmail.com'; // Enter Your E-mail
 	$from = 'admin@you-site-name.com'; // Admin e-mail
 	$fromName = 'Your Company Name'; // Your company name
 	$charset = 'UTF-8';
